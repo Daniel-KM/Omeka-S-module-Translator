9835dcc52551d6957128eac63ca6a709ce046382
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-06-01 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/ViewHelper/TranslationFactory.php
+++ src/Service/ViewHelper/TranslationFactory.php
@@ -2,7 +2,7 @@
 
 namespace Translator\Service\ViewHelper;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Translator\View\Helper\Translation;
 
